eb0bf97e32b7a69fdce6de28691f3a404e7bdcc9
Ilya <svaikinilya@yandex.ru>
2022-10-26 23:25:31 +0300

    Add lightOn and lightOff for ioPorts

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+# import RPi.GPIO as IO
+# import time
+
 ioPorts = [3, 5, 7, 8, 10, 11, 12, 13, 15, 16, 18, 19, 21, 22, 23, 24, 26, 29, 31, 32, 33, 35, 36, 37, 38, 40]
 groundPorts = [6, 9, 14, 20, 25, 30, 34, 39]
 uselessPorts = [1, 2, 4, 17, 27, 28]
@@ -17,7 +20,6 @@ def check():
         exit(1)
 
 
-# При exit RPi.GPIO.clear()
 class IoPort(object):
     def __init__(self, numPort):
         if (numPort not in ioPorts) or (numPort in inUsePorts):
@@ -25,17 +27,27 @@ class IoPort(object):
             exit(2)
         self.__ioPort = numPort
         inUsePorts.append(numPort)
+        # IO.setup(self.__ioPort, IO.OUT)
+        # IO.output(self.__ioPort, 0)
 
     def get(self):
-        if not (self.__ioPort in ioPorts):
+        if not (self.__ioPort in inUsePorts):
             print("ПОРТ НЕПРАВИЛЬНО УКАЗАН ВСЁ СГОРИТ (Get)\n")
             exit(2)
         return self.__ioPort
 
+    def lightOn(self):
+        print('Порт номер ', self.__ioPort, ' светится\n')
+        # IO.output(self.__ioPort, 1)
+
+    def lightOff(self):
+        print('Порт номер ', self.__ioPort, ' мрак\n')
+        # IO.output(self.__ioPort, 0)
+
 
 def main():
     check()
-
+    # IO.setmode(IO.BOARD)
     hoursPorts = []
     minutesPorts = []
 
@@ -47,9 +59,6 @@ def main():
     if len(hoursPorts) != 12 and len(minutesPorts) != 12:
         print("ПОРТОВ МНОГО ИЛИ МАЛО РАЗБЕРИСЬ\n (12)")
         exit(3)
-
-
-
 
     return 0
 
