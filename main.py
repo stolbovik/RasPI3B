8b8a62a9be622ea9dcc85e49d681a88e865b267f
stolbovik <stolbov_svyatoslav_02@mail.ru>
2022-10-28 00:41:05 +0300

    work clock in main

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 # import RPi.GPIO as IO
+import datetime
+import math
 import time
 
 ioPorts = [3, 5, 7, 8, 10, 11, 12, 13, 15, 16, 18, 19, 21, 22, 23, 24, 26, 29, 31, 32, 33, 35, 36, 37, 38, 40]
@@ -141,7 +143,6 @@ def timer(ports, start_minutes, start_seconds):
     # Вызов звукового сигнала TODO
 
 
-
 def secundomer():
     ports = []
     sec = 0
@@ -165,19 +166,49 @@ def secundomer():
                 debugShow(ports)
 
 
+class Clock:
+
+    def __init__(self, io_ports_for_clock):
+        self.__hourNow = datetime.datetime.now().time().hour
+        self.__minuteNow = datetime.datetime.now().time().minute
+        self.__secondNow = datetime.datetime.now().time().second
+        self.__IoPorts = io_ports_for_clock
+        self.startClock()
+
+    def startClock(self):
+        while 1:
+            temp_hours = self.__IoPorts[self.__hourNow % 12]
+            if not self.__IoPorts[self.__hourNow % 12].isLightOn():
+                self.__IoPorts[self.__hourNow % 12].lightOn()
+            if self.__hourNow % 12 != math.floor(self.__minuteNow / 5):
+                self.__IoPorts[math.floor(self.__minuteNow / 5)].lightOn()
+            debugShow(self.__IoPorts)
+            time.sleep(0.5)
+            if self.__hourNow % 12 != math.floor(self.__minuteNow / 5):
+                self.__IoPorts[math.floor(self.__minuteNow / 5)].lightOff()
+            debugShow(self.__IoPorts)
+            time.sleep(0.5)
+            self.__minuteNow = datetime.datetime.now().time().minute
+            self.__hourNow = datetime.datetime.now().time().hour
+            if self.__IoPorts[self.__hourNow % 12].get() != temp_hours.get():
+                temp_hours.lightOff()
+
+
 def main():
-    secundomer()
+    # secundomer()
     check()
     # IO.setmode(IO.BOARD)
     ports = []
 
-    # for i in range(0, 12):
-    #     ports.append(IoPort(ioPorts[i]))
-    # if len(ports) != 12:
-    #     print("ПОРТОВ МНОГО ИЛИ МАЛО РАЗБЕРИСЬ\n (12)")
-    #     exit(3)
+    for i in range(0, 12):
+        ports.append(IoPort(ioPorts[i]))
+    if len(ports) != 12:
+        print("ПОРТОВ МНОГО ИЛИ МАЛО РАЗБЕРИСЬ\n (12)")
+        exit(3)
 
-    debugShow(ports)
+    # debugShow(ports)
+
+    cl = Clock(ports)
 
     return 0
 
