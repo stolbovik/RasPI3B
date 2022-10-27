26f4412078b03c30fe3cdc460b4fc5a6fd86e7c4
Ilya <svaikinilya@yandex.ru>
2022-10-27 18:22:02 +0300

    resolved commits

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 # import RPi.GPIO as IO
 import time
 
-= [3, 5, 7, 8, 10,
-ioPorts 11, 12, 13, 15, 16, 18, 19, 21, 22, 23, 24, 26, 29, 31, 32, 33, 35, 36, 37, 38, 40]
+ioPorts = [3, 5, 7, 8, 10,
+           11, 12, 13, 15, 16, 18, 19, 21, 22, 23, 24, 26, 29, 31, 32, 33, 35, 36, 37, 38, 40]
 groundPorts = [6, 9, 14, 20, 25, 30, 34, 39]
 uselessPorts = [1, 2, 4, 17, 27, 28]
 inUsePorts = []
@@ -143,7 +143,6 @@ def timer(ports, start_minutes, start_seconds):
     # Вызов звукового сигнала TODO
 
 
-
 def secundomer():
     ports = []
     sec = 0
@@ -153,11 +152,10 @@ def secundomer():
         print("ПОРТОВ МНОГО ИЛИ МАЛО РАЗБЕРИСЬ\n (12)")
         exit(3)
 
-    ooooo
     timer(ports, 1, 3)
 
     while True:
-        for i in range (0, 12):
+        for i in range(0, 12):
             for j in range(0, 5):
                 ports[i].lightOn()
                 debugShow(ports)
@@ -169,17 +167,17 @@ def secundomer():
 
 
 def main():
-    secundomer()
     check()
     # IO.setmode(IO.BOARD)
     ports = []
+    for i in range(0, 12):
+        ports.append(IoPort(ioPorts[i]))
+    if len(ports) != 12:
+        print("ПОРТОВ МНОГО ИЛИ МАЛО РАЗБЕРИСЬ\n (12)")
+        exit(3)
 
-    # for i in range(0, 12):
-    #     ports.append(IoPort(ioPorts[i]))
-    # if len(ports) != 12:
-    #     print("ПОРТОВ МНОГО ИЛИ МАЛО РАЗБЕРИСЬ\n (12)")
-    #     exit(3)
-
+    # secundomer()
+    timer(ports, 1, 10)
     debugShow(ports)
 
     return 0
